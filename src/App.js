a5a8a57e3c8d2e2b6d5f8de571c5c3f59b853d85
Anzhelika Bielykh <anjelika.belikh@gmail.com>
2025-11-20 23:09:36 +0200

    Class component Todos list. Component Lifecycle.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import CounterComponent from "./CounterComponent";
 import ListComponent from "./ListComponent";
 import RenderComponent from "./RenderComponent";
+import MyClassComponent from "./MyClassComponent";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [isShowTimer, setIsShowTimer] = useState(false);
+  // const isShowTimer = false;
   return (
     <div className="App">
       <header className="App-header">
         {/* <RenderComponent /> */}
-        <ListComponent />
         {/* <CounterComponent /> */}
+        {isShowTimer ? <MyClassComponent /> : <ListComponent />}
+        <button onClick={() => setIsShowTimer((prev) => !prev)}>Show timer</button>
       </header>
     </div>
   );
--- src/MyClassComponent.js
+++ src/MyClassComponent.js
@@ -1,15 +1,53 @@
 import { Component } from "react";
 class MyClassComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "It is new Class component" };
+  state = {
+    todos: [],
+    input: "",
+    timer: 0,
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({ timer: prevState.timer + 1 }));
+    }, 1000);
+    const lsTodos = localStorage.getItem("todos");
+    if (lsTodos) {
+      this.setState({ todos: JSON.parse(lsTodos) });
+    }
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      console.log("componentDidUpdate");
+      localStorage.setItem("todos", JSON.stringify(this.state.todos));
+    }
+  }
+
+  addTask = () => {
+    this.setState({ todos: [...this.state.todos, this.state.input] });
+    this.setState({ input: "" });
+  };
+
+  onChangeHandler = (e) => {
+    const value = e.target.value;
+    this.setState({ input: value });
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.name}</h1>
-      </div>
+      <>
+        <h2>{this.state.timer}</h2>
+        <input type="text" value={this.state.input} onChange={this.onChangeHandler} />
+        <button onClick={this.addTask}>Add Todo</button>
+        {this.state.todos.map((todo, index) => (
+          <p key={index}>{todo}</p>
+        ))}
+      </>
     );
   }
 }
